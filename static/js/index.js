f62017f63b9b178d6068b2e1d3463fe23c47e119
John McLear <john@mclear.co.uk>
2020-11-22 17:45:42 +0000

    autofixes from Etherpad checkPlugins.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,18 @@
-exports.aceEditorCSS = function(){
+exports.aceEditorCSS = function () {
   return ['ep_show_whitespace/static/css/editor.css'];
 };
 
-exports.aceEditEvent = function(hook_name, args){
-  if(!args.callstack.docTextChanged) return false; // we should only run this if the pad contents is changed..
-  var divs = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").children("div");
-  $(divs).each(function(){
-    var text = $(this).text(); // get the text
-    var ch = text.charAt( text.length-1 ); // what's the last char of the string?
-    var hasTrailingWhitespace = (ch.match(/^\s{0,2}/)[0].length); // see if we have trailing whitespace
-    if(hasTrailingWhitespace){
-      $(this).css({"background-color":"red"}); // make it red
-    }else{
-      if($(this).css("background-color") === "red"){ // if it's red
-        $(this).css({"background-color":auto}); // remove it's color
-      }
+exports.aceEditEvent = function (hook_name, args) {
+  if (!args.callstack.docTextChanged) return false; // we should only run this if the pad contents is changed..
+  const divs = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').children('div');
+  $(divs).each(function () {
+    const text = $(this).text(); // get the text
+    const ch = text.charAt(text.length - 1); // what's the last char of the string?
+    const hasTrailingWhitespace = (ch.match(/^\s{0,2}/)[0].length); // see if we have trailing whitespace
+    if (hasTrailingWhitespace) {
+      $(this).css({'background-color': 'red'}); // make it red
+    } else if ($(this).css('background-color') === 'red') { // if it's red
+      $(this).css({'background-color': auto}); // remove it's color
     }
   });
-}
+};
