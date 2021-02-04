9390747a1582df750409411237c91f7d75acf912
John McLear <john@mclear.co.uk>
2021-02-04 11:44:17 +0000

    lint

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,26 @@
-exports.aceEditorCSS = function () {
-  return ['ep_show_whitespace/static/css/editor.css'];
-};
+'use strict';
 
-exports.aceEditEvent = function (hook_name, args) {
-  if (!args.callstack.docTextChanged) return false; // we should only run this if the pad contents is changed..
-  const divs = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').children('div');
+exports.aceEditorCSS = () => ['ep_show_whitespace/static/css/editor.css'];
+
+exports.aceEditEvent = (hookName, args) => {
+  // we should only run this if the pad contents is changed..
+  if (!args.callstack.docTextChanged) return false;
+  const divs = $('iframe[name="ace_outer"]').contents()
+      .find('iframe').contents().find('#innerdocbody').children('div');
   $(divs).each(function () {
-    const text = $(this).text(); // get the text
-    const ch = text.charAt(text.length - 1); // what's the last char of the string?
-    const hasTrailingWhitespace = (ch.match(/^\s{0,2}/)[0].length); // see if we have trailing whitespace
+    // get the text
+    const text = $(this).text();
+    // what's the last char of the string?
+    const ch = text.charAt(text.length - 1);
+    // see if we have trailing whitespace
+    const hasTrailingWhitespace = (ch.match(/^\s{0,2}/)[0].length);
     if (hasTrailingWhitespace) {
-      $(this).css({'background-color': 'red'}); // make it red
-    } else if ($(this).css('background-color') === 'red') { // if it's red
-      $(this).css({'background-color': auto}); // remove it's color
+      // make it red
+      $(this).css({'background-color': '#ffe1e1'});
+      // if it's red
+    } else if ($(this).css('background-color') === '#ffe1e1') {
+      // remove it's color
+      $(this).css({'background-color': 'auto'});
     }
   });
 };
